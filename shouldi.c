8e7902b0aecf9d74c0c61b174ec2a78b092d493a
John Reed <johnnycarlos@gmail.com>
2013-11-14 07:59:40 -0800

    comments and formatting

--- shouldi.c
+++ shouldi.c
@@ -3,20 +3,22 @@
  
 int main()
 {
-
     /* put these in another file? */
     const char* yes[] = { "Yes.", "Oh yea." };
     const char* no[] = { "No.", "No way." };
 
     /* seed rand */
     srand( time(NULL) );
-
+    
+    /* random 0 or 1 */
     int i = rand() % 2;
 
     if( i == 1 ) {
         puts( yes[0] );
-    } else {
+    }
+    else {
         puts( no[0] );
     }
+
     return 0;
 }
