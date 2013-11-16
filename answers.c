c6da2ff05c2d6d9f1e77f8928b0e3cc6b4c92022
John Reed <johnnycarlos@gmail.com>
2013-11-15 21:32:47 -0800

    added function to calculate yes/no array length

--- answers.c
+++ answers.c
@@ -1,13 +1,19 @@
 #include "answers.h"
 
 char* yes[] = { 
-"Yes.", 
-"Oh yea."
+"Yes", 
+"Oh yes",
+"Oh yea",
+"Yea",
+"Yup",
+"Heck yea!",
+'\0' /* adding null because I don't yet know how to sizeof char *[], so I loop and count */
 };
 
 
 
 char* no[] = { 
 "No.",
-"No way."
+"No way.",
+'\0' /* adding null because I don't yet know how to sizeof char *[], so I loop and count */
 };
--- shouldi.c
+++ shouldi.c
@@ -2,14 +2,21 @@
 #include <time.h>
 #include "answers.h"
 
+int array_length(char **);
+
 /* TODO 
-     - calculate the current length of answer arrays
      - two more rand variables to use for picking random answers
      - add more answsers
 */
 
 int main()
 {
+
+    int yes_length = array_length( yes );
+    printf("length of yes: %d \n", yes_length);
+
+    int no_length = array_length( no );
+    printf("length of no: %d \n", no_length);
 
     /* seed rand */
     srand( time(NULL) );
@@ -18,11 +25,24 @@ int main()
     int i = rand() % 2;
 
     if( i == 1 ) {
-        puts( yes[0] );
+       // puts( yes[0] );
     }
     else {
-        puts( no[0] );
+      //  puts( no[0] );
     }
 
     return 0;
+}
+
+/* This function counts the number of elements in a char *[],
+   until I learn a better way */
+int array_length(char **answer){
+    /* puts( answer[1] ); */
+    int count = 0;
+    while( *answer != '\0' ){
+        /* puts( *answer ); */
+        answer++;
+        count++;
+    } 
+    return count;
 }
